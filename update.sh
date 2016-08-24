fdb5a2836bd1fcd6d76aaffb2d5c8fc22e011697
allenkay327 <allenkay327@gmail.com>
2016-08-24 18:37:19 +0900

    Update news

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 if which pyenv > /dev/null 2> /dev/null; then eval "$(pyenv init -)"; fi
 pyenv activate femlibrary
 
-echo "Crawl..."
+echo "Crawl news..."
 cd ../news-crawl
 git pull
 python crawl.py
@@ -11,6 +11,9 @@ echo "Categorize..."
 cd ../femlibrary-src
 git pull
 python json2csv.py > _data/news.csv
+
+echo "Download books..."
+wget "https://docs.google.com/spreadsheets/d/1N9sUaBOqTMRzzecl5RHiBLUEBf9wJ-SjGkb3dtPNdr8/export?format=csv&id=1N9sUaBOqTMRzzecl5RHiBLUEBf9wJ-SjGkb3dtPNdr8&gid=270197918" --no-check-certificate -q -O _data/books.csv
 
 echo "Generate site..."
 jekyll b
