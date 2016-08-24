bdfae56312f37d5877851b1ef3842c465bb60952
allenkay327 <allenkay327@gmail.com>
2016-08-24 16:54:15 +0900

    Update news

--- update.sh
+++ update.sh
@@ -2,8 +2,27 @@
 if which pyenv > /dev/null 2> /dev/null; then eval "$(pyenv init -)"; fi
 pyenv activate femlibrary
 
-echo "Crawling news..."
+echo "Crawl..."
 cd ../news-crawl
 git pull
 python crawl.py
+
+echo "Categorize..."
+cd ../femlibrary-src
+git pull
+python json2csv.py > _data/news.csv
+
+echo "Generate site..."
+jekyll b
+
+echo "Push..."
+cd ../femlibrary.github.io
+git pull
+cp -r ../femlibrary-src/_site/* .
+git add .
+git commit -m "Update news"
+git push
+
+cd ../femlibrary-src
+echo "Done!"
 
